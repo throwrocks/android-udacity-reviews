81eacd36bda5a10b677c5eeceb07cbadea3ee5c8
Jose E Lopez <p0p.thr0w@gmail.com>
2016-08-20 14:46:17 -0500

    feat: turned off Debug mode (sorry).

--- app/src/main/java/rocks/athrow/android_udacity_reviews/activity/MainActivity.java
+++ app/src/main/java/rocks/athrow/android_udacity_reviews/activity/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        boolean DEBUG = true;
+        boolean DEBUG = false;
 
         setContentView(R.layout.activity_main);
 
