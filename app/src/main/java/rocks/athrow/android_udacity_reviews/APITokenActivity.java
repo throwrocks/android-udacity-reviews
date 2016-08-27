3915d12acbf09aafbf06746921879a786e9344d0
rabyunghwa <rabyunghwa@gmail.com>
2016-08-27 22:46:23 +0800

    first commit

---
+++ app/src/main/java/rocks/athrow/android_udacity_reviews/APITokenActivity.java
@@ -0,0 +1,13 @@
+package rocks.athrow.android_udacity_reviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APITokenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apitoken);
+    }
+}
